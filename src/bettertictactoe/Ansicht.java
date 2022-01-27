0ef19df47ec8875002d7cb6f57c72bd57f5177ef
lucyjin1231 <60175855+lucyjin1231@users.noreply.github.com>
2022-01-27 16:45:35 +0100

    Add files via upload

---
+++ src/bettertictactoe/Ansicht.java
@@ -0,0 +1,13 @@
+package bettertictactoe;
+public class Ansicht {
+
+	public Ansicht() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module bettertictactoe {
+}
